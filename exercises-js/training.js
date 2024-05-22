427de3917f388e756c45e3ea0449a358a6eb028e
bruno-ssilveira <129340450+bruno-ssilveira@users.noreply.github.com>
2024-05-22 10:18:58 -0300

    new

---
+++ exercises-js/training.js
@@ -0,0 +1,18 @@
+console.log("testing");
+
+// In Js are not distinguishment between an integer and a double/float
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+let salario = 4578.32;
+console.log(typeof salario);
+
+const constante = 3; //can't be modified
+console.log(constante);
+
+let stringTest = 'Js';
+console.log('Escola '.concat(stringTest).concat('!'));
+
+console.log(stringTest.replace('s', 'a'));
+
+console.log('Ana,Maria,Pedro'.split(','));
